7c9c20b1ea689234acecde3b2545a14fd68a5a39
hainh3 <hainh3@smartosc.com>
2023-12-15 17:21:41 +0700

    test3

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,5 +2,5 @@ from django.urls import path,include
 from . import  views
 urlpatterns = [
     path('',views.index,name='index'),
-    path('test',views.test,name='test'),
+    path('test222222222',views.test,name='test'),
 ]
